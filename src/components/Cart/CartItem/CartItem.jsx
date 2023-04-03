4b9730703b205ef219bc6ae25c321b3aa9c3b3dd
Omid_G12 <omid.12.ghari@gmail.com>
2023-04-03 11:40:20 -0700

    cart actions UI created

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -13,6 +13,14 @@ const CartItem = ({item}) => {
         <Typography variant='h4'>{item.name}</Typography>
         <Typography variant='h5'>{item.line_total.formatted_with_symbol}</Typography>
       </CardContent>
+      <CardActions className={classes.cartActions}>
+        <div className={classes.buttons}>
+          <Button type='button' size='small'>-</Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type='button' size='small'>+</Button>
+        </div>
+        <Button variant='contained' type='button' color='secondary'>Remove</Button>
+      </CardActions>
     </Card>
   )
 }
