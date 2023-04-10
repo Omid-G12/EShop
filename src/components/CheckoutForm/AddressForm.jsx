7a5fa76b3499b240fe185c19fe1a1cd9dc1dfe82
Omid_G12 <omid.12.ghari@gmail.com>
2023-04-10 14:42:16 -0700

    shipping address form functionality completed

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -5,8 +5,9 @@ import  { useForm, FormProvider } from 'react-hook-form';
 import {commerce} from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
+import { Link } from 'react-router-dom';
 
-const AddressForm = ({checkoutToken}) => {
+const AddressForm = ({checkoutToken, next}) => {
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState('');
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -58,7 +59,7 @@ const AddressForm = ({checkoutToken}) => {
       <Typography variant='h6' gutterBottom>Shipping Address</Typography>
 
       <FormProvider {...methods}>
-        <form onSubmit=''>
+        <form onSubmit={methods.handleSubmit((data) => next({...data, shippingCountry, shippingSubdivision, shippingOption}))}>
           <Grid container justifyContent={'space-between'} >
             <FormInput name='firstName' label='First name' />
             <FormInput name='lastName' label='Last name' />
@@ -99,6 +100,11 @@ const AddressForm = ({checkoutToken}) => {
               </Select>
             </Grid>
           </Grid>
+          <br />
+          <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Button component={Link} to="/cart" variant='outlined'>Back to Cart</Button>
+            <Button type='submit' variant='contained' color='primary'>Next</Button>
+          </div>
         </form>
       </FormProvider>
     </>
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -12,6 +12,7 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = ({cart}) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
   const {classes} = useStyles();
 
   useEffect(() => {
@@ -28,11 +29,19 @@ const Checkout = ({cart}) => {
     generateToken();
   }, [cart]);
 
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+    nextStep();
+  }
+
   const Confirmation = () => <div>Confirmation</div>
 
   const Form = () => activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken}/>
-    : <PaymentForm />
+    ? <AddressForm checkoutToken={checkoutToken} next={next}/>
+    : <PaymentForm shippingData={shippingData}/>
 
   return (
     <>
