1ed8336a955a05177693f5361e9448a6b8dd3271
Omid_G12 <omid.12.ghari@gmail.com>
2023-04-12 00:45:37 -0700

    UI fixes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,7 +10,7 @@ const Products = ({ products, onAddToCart }) => {
   return (
     <main className={classes.content}>
       <div className={classes.toolbar}/>
-      <Grid container justifyContent={'center'} spacing={4} paddingTop={3}>
+      <Grid container justifyContent={'center'} spacing={4} paddingTop={10}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} onAddToCart={onAddToCart}/>
